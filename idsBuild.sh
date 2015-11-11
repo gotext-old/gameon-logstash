c330be25c1dd8598b2bf4469130ac9172f692988
Ben Smith <benjsmi@us.ibm.com>
2015-11-11 15:26:36 -0500

    Open up syslog as another way.

--- idsBuild.sh
+++ idsBuild.sh
@@ -20,4 +20,4 @@ echo Building the docker image...
 ./docker build -t gameon-logstash .
 ./docker stop -t 0 gameon-logstash
 ./docker rm gameon-logstash
-./docker run -d -p 5043:5043 --name gameon-logstash gameon-logstash
+./docker run -d -p 5043:5043 -p 514:514 --name gameon-logstash gameon-logstash
