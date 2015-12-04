d42da6b45414f88dbf1b14b46e44134b53d6b59a
Ben Smith <benjsmi@us.ibm.com>
2015-12-04 13:15:32 -0500

    Whoops; forgot to update the ids build.

--- idsBuild.sh
+++ idsBuild.sh
@@ -10,7 +10,8 @@ mkdir dockercfg ; cd dockercfg
 echo Downloading Docker requirements..
 wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/dockerneeds.tar -q
 echo Setting up Docker...
-sudo apt-get update ; sudo apt-get install -y lxc
+wget http://security.ubuntu.com/ubuntu/pool/main/a/apparmor/libapparmor1_2.8.95~2430-0ubuntu5.3_amd64.deb -O libapparmor.deb
+sudo dpkg -i libapparmor.deb
 tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
 	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2376" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
 
@@ -18,9 +19,7 @@ echo Setting configuration to match with your Bluemix Org and Space information 
 ./client-config.sh $BLUEMIX_USER $BLUEMIX_PASSWORD $BLUEMIX_ORG $BLUEMIX_SPACE
 
 echo Building the docker image...
-sed -i s/PLACEHOLDER_ADMIN_PASSWORD/$ADMIN_PASSWORD/g Dockerfile
-
 ./docker build -t gameon-logstash .
 ./docker stop -t 0 gameon-logstash
 ./docker rm gameon-logstash
-./docker run -d -p 10.33.40.21:5043:5043 -p 10.33.40.21:514:514/udp --name gameon-logstash gameon-logstash
+./docker run -d -p 10.33.40.21:5043:5043 -p 10.33.40.21:514:514/udp -e LOGSTASH_CERT="$LOGSTASH_CERT" -e LOGSTASH_KEY="$LOGSTASH_KEY" --name gameon-logstash gameon-logstash
