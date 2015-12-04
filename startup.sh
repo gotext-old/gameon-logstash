2961e8af5a977b022a63fa6f5dfdce3eba41e7ba
Ben Smith <benjsmi@us.ibm.com>
2015-12-04 13:30:18 -0500

    Then the startup script needs fixing.

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ echo Starting logstash...
 echo -e $LOGSTASH_CERT > /opt/logstash-forwarder.crt
 echo -e $LOGSTASH_KEY > /opt/logstash-forwarder.key
 sleep 0.5
-JAVA_HOME=/opt/jdk1.8.0_65 /opt/logstash/bin/logstash -f /opt/logstash/bin/logstash.conf
+JAVA_HOME=/opt/jdk1.8.0_66 /opt/logstash/bin/logstash -f /opt/logstash/bin/logstash.conf
