f8c8261bf556d87f41b6e064f6e9fd01ea7df50f
Ben Smith <benjsmi@us.ibm.com>
2016-01-04 14:12:31 -0500

    Full path required.

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ rm etcd-v2.2.2-linux-amd64.tar.gz
 mv etcdctl /usr/local/bin/etcdctl
 
 echo Starting logstash...
-etcdctl get /logstash/cert > logstash-forwarder.crt
-etcdctl get /logstash/key > logstash-forwarder.key
+etcdctl get /logstash/cert > /opt/logstash-forwarder.crt
+etcdctl get /logstash/key > /opt/logstash-forwarder.key
 sleep 0.5
 JAVA_HOME=/opt/jdk1.8.0_66 /opt/logstash/bin/logstash -f /opt/logstash/bin/logstash.conf
