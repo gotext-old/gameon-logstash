f7492dbf3331a101809aecf01e75be8c30d0f696
Erin Schnabel <ebullient@users.noreply.github.com>
2016-08-29 21:53:23 -0400

    Docker host move

--- idsBuild.sh
+++ idsBuild.sh
@@ -15,7 +15,7 @@ cd ..
 wget http://security.ubuntu.com/ubuntu/pool/main/a/apparmor/libapparmor1_2.8.95~2430-0ubuntu5.3_amd64.deb -O libapparmor.deb
 sudo dpkg -i libapparmor.deb
 rm libapparmor.deb
-wget https://get.docker.com/builds/Linux/x86_64/docker-1.9.1 --quiet -O docker
+wget https://get.docker.com/builds/Linux/x86_64/docker-1.12.1 --quiet -O docker
 chmod +x docker
 
 
@@ -27,6 +27,6 @@ sed -i s/PLACEHOLDER_DOWNLOAD_PASSWORD/$DOWNLOAD_PASSWORD/g ./Dockerfile
 ./docker build -t gameon-logstash .
 ./docker stop -t 0 gameon-logstash
 ./docker rm gameon-logstash
-./docker run -d -p 10.33.40.21:5043:5043 -p 10.33.40.21:514:514/udp -e ETCDCTL_ENDPOINT="http://etcd:4001" --link=etcd --name gameon-logstash gameon-logstash
+./docker run -d -p $SL_HOST_IP:5043:5043 -p $SL_HOST_IP:514:514/udp -e ETCDCTL_ENDPOINT="http://etcd:4001" --link=etcd --name gameon-logstash gameon-logstash
 
 rm -rf dockercfg
